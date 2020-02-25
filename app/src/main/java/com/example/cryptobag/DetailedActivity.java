db753f5d15828ad440d8d70e2688cd550c799e08
Aditya Nayanar <adityavn08@gmail.com>
2020-02-25 22:59:16 +1100

    Added Video

--- app/src/main/java/com/example/cryptobag/DetailedActivity.java
+++ app/src/main/java/com/example/cryptobag/DetailedActivity.java
@@ -2,7 +2,11 @@ package com.example.cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.net.Uri;
 
 public class DetailedActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class DetailedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
+
+        Button toVideo = findViewById(R.id.toVideo);
+        toVideo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=mWsvsE27nOY"));
+                startActivity(intent);
+            }
+        });
     }
 }
