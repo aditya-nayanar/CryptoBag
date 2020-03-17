c07b28ef6ba06a0e7065c8809ec0dcf5d99d03be
Aditya <adityavn08@gmail.com>
2020-03-17 14:46:32 +1100

    Finished Week 5 Fragments - DetailActivity now populates DetailFragment

--- app/src/main/java/com/example/cryptobag/DetailedActivity.java
+++ app/src/main/java/com/example/cryptobag/DetailedActivity.java
@@ -1,7 +1,12 @@
 package com.example.cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,47 +21,28 @@ import java.util.List;
 public class DetailedActivity extends AppCompatActivity {
 private static final String TAG = "DetailedActivity";
     private final List<Coin> mCoinList = Coin.getCoins();
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
 
         Intent intent = getIntent();
-        Coin coin = null;
+        int mPosition = 0;
         if(intent.getExtras() != null)
 
         {
             Bundle extra = intent.getExtras();
-            coin = mCoinList.get(extra.getInt("pos"));
+            mPosition = extra.getInt("pos");
         }
 
-        Coin selectedCoin = coin;
-
-        TextView name = findViewById(R.id.name);
-        name.setText(selectedCoin.getName());
-        TextView symbol = findViewById(R.id.symbol);
-        symbol.setText(selectedCoin.getSymbol());
-        TextView value = findViewById(R.id.value);
-        value.setText(Double.toString(selectedCoin.getValue()));
-        TextView change1h = findViewById(R.id.change1h);
-        change1h.setText(Double.toString(selectedCoin.getChange1h()));
-        TextView change24h = findViewById(R.id.change24h);
-        change24h.setText(Double.toString(selectedCoin.getChange24h()));
-        TextView change7d = findViewById(R.id.change7d);
-        change7d.setText(Double.toString(selectedCoin.getChange7d()));
-        TextView marketcap = findViewById(R.id.marketcap);
-        marketcap.setText(Double.toString(selectedCoin.getMarketcap()));
-        TextView volume = findViewById(R.id.volume);
-        volume.setText(Double.toString(selectedCoin.getVolume()));
-        final String searchvalue = selectedCoin.getName();
-
-        ImageButton search = findViewById(R.id.searchButton);
-        search.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://www.google.com.au/search?q="+searchvalue+" Cryptocurrency rate"));
-                startActivity(intent);
-            }
-        });
+        DetailFragment fragment = new DetailFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("pos", mPosition);
+        fragment.setArguments(bundle);
+        FragmentManager myManager = getSupportFragmentManager();
+        FragmentTransaction myTransaction = myManager.beginTransaction();
+        myTransaction.replace(R.id.tablet_coinDetails, fragment);
+        myTransaction.commit();
     }
 }
