eafa80508da3a3ba3931f8bf7d4fb64328b060c3
Aditya <adityavn08@gmail.com>
2020-04-14 14:00:54 +1000

    Week 8 - Networking: Completed (added notifydatasetchange)

--- app/src/main/java/com/example/cryptobag/MainActivity.java
+++ app/src/main/java/com/example/cryptobag/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Button;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import com.example.cryptobag.Entities.Coin;
 import com.example.cryptobag.Entities.CoinLoreResponse;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,12 +33,6 @@ public class MainActivity extends AppCompatActivity {
     List<Coin> newCoinList = CoinLoreResponse.getCoinList();
     List<Coin> secondList = new ArrayList<>();
 
-    Retrofit rf = new Retrofit.Builder()
-            .baseUrl("https://api.coinlore.net/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-    CoinService cs = rf.create(CoinService.class);
-
     private RecyclerView currencyList;
     private CoinListAdapter cAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -48,39 +44,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Call<CoinLoreResponse> coinListCall = cs.getCoinList();
-        coinListCall.enqueue(new Callback<CoinLoreResponse>() {
-            @Override
-            public void onResponse(Call<CoinLoreResponse> call, Response<CoinLoreResponse> response){
-                if(response.isSuccessful()){
-                    CoinLoreResponse clr = response.body();
-                    secondList = clr.getData();
-                    System.out.println(secondList.size());
-
                     currencyList = findViewById(R.id.coinList);
                     if(currencyList != null) {
                         // Create an adapter and supply the data to be displayed.
-                        cAdapter = new CoinListAdapter(MainActivity.this, newCoinList);
-                        System.out.println(secondList.size());
-                        cAdapter.setData(secondList);
-                        Toast.makeText(MainActivity.this, "Coin list has been refreshed", Toast.LENGTH_SHORT).show();
+                        cAdapter = new CoinListAdapter(MainActivity.this, new ArrayList<>());
+                        new MyTask().execute();
+
                         // Connect the adapter with the RecyclerView.
                         currencyList.setAdapter(cAdapter);
                         // Give the RecyclerView a default layout manager.
                         currencyList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
                     }
+    }
 
-                }
-            }
+    public class MyTask extends AsyncTask<Void, Integer, Integer>{
 
-            @Override
-            public void onFailure(Call<CoinLoreResponse> call, Throwable t){
+        Retrofit rf = new Retrofit.Builder()
+                .baseUrl("https://api.coinlore.net/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        CoinService cs = rf.create(CoinService.class);
+        List<Coin> newCoinList;
 
-            }
-        });
+        @Override
+        protected Integer doInBackground(Void... voids) {
+        try {
+            Call<CoinLoreResponse> coinListCall = cs.getCoinList();
+            Response<CoinLoreResponse> clr = coinListCall.execute();
+            newCoinList = clr.body().getData();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+            return null;
+        }
 
-        // Get a handle to the RecyclerView.
-
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            cAdapter.setData(newCoinList);
+            cAdapter.notifyDataSetChanged();
+            Toast.makeText(MainActivity.this, "Coin list has been refreshed", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
